16e952e01cc7446d54d4ca353ece107b422a20b3
genevieveluyt <indigomelody@hotmail.com>
2015-02-21 12:49:41 -0800

    wrote handleStart

--- src/main/java/BattleSnakeHandlers.java
+++ src/main/java/BattleSnakeHandlers.java
@@ -9,10 +9,10 @@ public class BattleSnakeHandlers {
     public Object handleStart(Map<String, Object> requestBody) {
         // Dummy Response
         Map<String, Object> responseObject = new HashMap<String, Object>();
-        responseObject.put("name", "battlesnake-java");
-        responseObject.put("color", "#ff0000");
-        responseObject.put("head_url", "http://battlesnake-java.herokuapp.com/");
-        responseObject.put("taunt", "battlesnake-java");
+        responseObject.put("name", "wecsssssnake");
+        responseObject.put("color", "#00BFFF");
+        responseObject.put("head_url", ""http://www.mapleprimes.com/DocumentFiles/95819/279486/pacman-moving.gif");
+        responseObject.put("taunt", "Omnomnomnom");
         return responseObject;
     }
     
